9eda0662b181c09cf303d852e21f4063aead13e8
ChernyshevaNetology <chernysheva87@mail.ru>
2020-08-25 19:27:13 +0200

    homework

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,24 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+function getResult(a,b,c) {
+    let result = b ** 2 - 4 * a * c;
+
+    if (result < 0) {
+        return [];
+    } else if(result === 0) {
+        return [-b/2*a];
+    } else if(result > 0) {
+        return [(-b + Math.sqrt(result))/2*a, (-b - Math.sqrt(result))/2*a];
+    }
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    if (!marks.length) {
+        return 0;
+    }
+  const result = marks.length > 5 ? marks.slice(0,5) : marks;
+
+  return result.reduce((a,e) => a + e) / result.length;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+function askDrink(name, dateOfBirthday) {
+
 }
