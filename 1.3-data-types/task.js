1a018a34971bd59fb2baea9debdf7efaf56fc43b
ChernyshevaNetology <chernysheva87@mail.ru>
2020-08-26 18:56:51 +0200

    homework commit

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,34 @@
-function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+// вспомогательная функция для перевода года в месяцы
+const monthsParser = (date) =>
+  (date.getFullYear() - new Date().getFullYear()) * 12;
+
+function calculateTotalMortgage(
+  percent = 10,
+  contribution = 0,
+  amount,
+  date = 12
+) {
+  const values = [percent, contribution, amount, monthsParser(date)];
+
+  for (const value of values) {
+    if (Number.isNaN(parseInt(value))) {
+      return `${value} содержит неправильное значение, введите число`;
+    }
+  }
+
+  const loanDuration = monthsParser(date);
+  const loanAmount = amount - contribution; // сумма кредита минус взнос
+  const monthlyInterest = percent / 100 / 12; // процентная ставка в месяц
+  const monthlyPayment =
+    loanAmount *
+    (monthlyInterest +
+      monthlyInterest / ((1 + monthlyInterest) ** loanDuration - 1)); // ежемесячный платеж
+
+  return +(monthlyPayment * loanDuration).toFixed(2);
 }
 
+// 2 задача
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+  const message = "Привет, мир! Меня зовут";
+  return !name ? `${message} Аноним` : `${message} ${name}`;
 }
